a19a799153f51a6ba94c6a12be3663a538f3210f
Dale Morris <dale.morris@wpengine.com>
2021-07-14 13:18:39 -0500

    test push

---
+++ .wpengine-conf/wp-config-site.php
@@ -0,0 +1,5 @@
+<?php
+# GENERATED AUTOMATICALLY ON 2021-07-14 10:55:14.751617 -0500 CDT m=+82.877233873
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "dalesptrain.wpengine.com";
+$wpengine_primary_original_ip_address = "35.237.149.96";
